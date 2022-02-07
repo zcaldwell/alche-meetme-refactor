cc0faa4d447a5d208a22b4e68649c86663a832c7
zcaldwell <mintyst.john@gmail.com>
2022-02-07 12:45:03 -0800

    page rendering!

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import Layout from './views/Layout/Layout'
 import Home from './views/Home/Home'
-import fetchUser from './services/user'
+import { UserProvider } from './context/UserContext'
 
 function App() {
   // inital value should match the data type of end value
-  const [user, setUser] = useState({})
-
-  useEffect(() => {
-    fetchUser()
-      .then((fetchedUser) => {
-        setUser(fetchedUser)
-      })
-      .catch((error) => {
-        throw new Error(`Error: ${error}`)
-      })
-  }, [])
 
   return (
-    <Layout user={user}>
-      <Home user={user} />
-    </Layout>
+    <UserProvider>
+      <Layout>
+        <Home />
+      </Layout>
+    </UserProvider>
   )
 }
 
